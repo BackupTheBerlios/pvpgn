22500415fbe6bb3bdf14e839dab6b1c1004df0d8
dizzy <dizzy>
2003-01-04 21:28:30 +0000

    Updated version to 1.2.3

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,8 +1,8 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
 #ifndef WITH_MYSQL
-#define PVPGN_VERSION "BnetD Mod 1.2.2"
+#define PVPGN_VERSION "BnetD Mod 1.2.3"
 #else
-#define PVPGN_VERSION "BnetD Mod 1.2.2 /w MySQL"
+#define PVPGN_VERSION "BnetD Mod 1.2.3 /w MySQL"
 #endif
 #endif
