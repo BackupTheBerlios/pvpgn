d0eea11a9b4edcaa015ca78086e905833853f2fa
Omega <Omega>
2003-01-15 09:23:33 +0000

    Updated to show cvs version

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,8 +1,8 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
 #ifndef WITH_MYSQL
-#define PVPGN_VERSION "BnetD Mod 1.2.3"
+#define PVPGN_VERSION "BnetD Mod 1.2.4-CVS"
 #else
-#define PVPGN_VERSION "BnetD Mod 1.2.3 /w MySQL"
+#define PVPGN_VERSION "BnetD Mod 1.2.4-CVS /w MySQL"
 #endif
 #endif
