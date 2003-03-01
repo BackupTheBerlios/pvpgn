9f8b6355fe1b12f8debd1ece65c4e615e2b684b6
dizzy <dizzy>
2003-03-01 23:11:01 +0000

    Updated version to the new 1.4.0 development branch

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,8 +1,8 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
 #ifndef WITH_MYSQL
-#define PVPGN_VERSION "BnetD Mod 1.3.0"
+#define PVPGN_VERSION "BnetD Mod 1.4.0-CVS"
 #else
-#define PVPGN_VERSION "BnetD Mod 1.3.0 /w MySQL"
+#define PVPGN_VERSION "BnetD Mod 1.4.0-CVS /w MySQL"
 #endif
 #endif
