2ab2e6b876df449133cccc91e4058052a4d61edf
dizzy <dizzy>
2003-03-11 07:37:42 +0000

    Development branch is 1.5.0 !! only the stable one is 1.4.1

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,8 +1,8 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
 #ifndef WITH_MYSQL
-#define PVPGN_VERSION "BnetD Mod 1.4.1-CVS"
+#define PVPGN_VERSION "BnetD Mod 1.5.0-CVS"
 #else
-#define PVPGN_VERSION "BnetD Mod 1.4.1-CVS /w MySQL"
+#define PVPGN_VERSION "BnetD Mod 1.5.0-CVS /w MySQL"
 #endif
 #endif
