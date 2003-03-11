25b27d4c36b6e73aaaa92e5eeffecba633d954f4
Omega <Omega>
2003-03-11 07:30:13 +0000

    Update version to 1.4.1 development branch

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,8 +1,8 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
 #ifndef WITH_MYSQL
-#define PVPGN_VERSION "BnetD Mod 1.4.0"
+#define PVPGN_VERSION "BnetD Mod 1.4.1-CVS"
 #else
-#define PVPGN_VERSION "BnetD Mod 1.4.0 /w MySQL"
+#define PVPGN_VERSION "BnetD Mod 1.4.1-CVS /w MySQL"
 #endif
 #endif
