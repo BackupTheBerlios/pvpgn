43ea41b2a3fe10dfffad1e54cd43fd8c167978e3
dizzy <dizzy>
2003-07-18 18:36:46 +0000

    Just increased revision to 1.5.0beta3 (in CVS we are working on this). beta3 should be the last beta before 1.5.0 final.

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,6 +1,6 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
-#define PVPGN_VERSION "1.5.0beta2"
+#define PVPGN_VERSION "1.5.0beta3"
 #endif
 #ifndef PVPGN_SOFTWARE
 #define PVPGN_SOFTWARE "PvPGN BnetD Mod"
