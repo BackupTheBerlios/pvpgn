8879cb7261e4f7db2436edc014e3eb68eabcea25
dizzy <dizzy>
2003-07-30 20:12:46 +0000

    Added infos for users to know how to enable CDB and using this borland script

--- pvpgn/src/make_borland.bat
+++ pvpgn/src/make_borland.bat
@@ -1,3 +1,4 @@
-#Parameter -DWITH_SQL_MYSQL enables SQL
+#Parameter -DWITH_SQL_MYSQL enables MySQL SQL storage support
+#Parameter -DWITH_CDB enables CDB file storage support
 #Parameter clean deletes object files
 make -fmakefile.BORLAND %1 %2
