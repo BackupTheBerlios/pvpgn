15e00af7a564e79dfe86b69d3a93673eca3e4ee1
dizzy <dizzy>
2003-09-09 18:02:16 +0000

    Preparing for 1.6.0pre1 (pre)release

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,6 +1,6 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
-#define PVPGN_VERSION "1.6.0-CVS"
+#define PVPGN_VERSION "1.6.0pre1"
 #endif
 #ifndef PVPGN_SOFTWARE
 #define PVPGN_SOFTWARE "PvPGN BnetD Mod"
