db1e0fcdcade1947270e05ae21fcd7e7db369873
dizzy <dizzy>
2003-12-20 08:49:49 +0000

    1.6.0pre3 released

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,6 +1,6 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
-#define PVPGN_VERSION "1.6.0pre3-CVS"
+#define PVPGN_VERSION "1.6.0pre3"
 #endif
 #ifndef PVPGN_SOFTWARE
 #define PVPGN_SOFTWARE "PvPGN BnetD Mod"
