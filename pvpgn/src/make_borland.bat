793b8254e1ec2648f4c97995aa898ab131e6abad
aaron <aaron>
2003-12-21 12:44:11 +0000

    just removed useless cdb stuff

--- pvpgn/src/make_borland.bat
+++ pvpgn/src/make_borland.bat
@@ -1,4 +1,3 @@
 #Parameter -DWITH_SQL_MYSQL enables MySQL SQL storage support
-#Parameter -DWITH_CDB enables CDB file storage support
 #Parameter clean deletes object files
 make -fmakefile.BORLAND %1 %2
