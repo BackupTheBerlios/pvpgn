063cbed111aca86420bfe0d25ad448450f9f7dd2
dizzy <dizzy>
2004-01-07 22:50:58 +0000

    Version in CVS now is 1.6.0pre5-CVS

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,6 +1,6 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
-#define PVPGN_VERSION "1.6.0pre4"
+#define PVPGN_VERSION "1.6.0pre5-CVS"
 #endif
 #ifndef PVPGN_SOFTWARE
 #define PVPGN_SOFTWARE "PvPGN BnetD Mod"
