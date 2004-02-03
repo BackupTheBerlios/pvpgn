ad8384dfdf7ac1fe6eba002e9cd20083f1aaba29
dizzy <dizzy>
2004-02-03 18:20:54 +0000

    Ok guys, we officially enter feature freeze before 1.6.0beta release. Only bugfixes allowed. Ill start spread this snap also to testers.

--- pvpgn/src/common/version.h
+++ pvpgn/src/common/version.h
@@ -1,6 +1,6 @@
 #include "setup_before.h"
 #ifndef PVPGN_VERSION
-#define PVPGN_VERSION "1.6.0pre6-CVS"
+#define PVPGN_VERSION "1.6.0beta-CVS"
 #endif
 #ifndef PVPGN_SOFTWARE
 #define PVPGN_SOFTWARE "PvPGN BnetD Mod"
