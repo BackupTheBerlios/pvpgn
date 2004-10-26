fa3b1f6825a32a75f82929afe9364b37758b8618
pandaemonium <pandaemonium>
2004-10-26 20:48:57 +0000

    made batch output better readable

--- pvpgn/Win32-Projects/make_borland.bat
+++ pvpgn/Win32-Projects/make_borland.bat
@@ -1,9 +1,9 @@
 @echo off
+echo ===================================================================
 echo Parameter -DWITH_SQL_MYSQL enables MySQL SQL storage support
 echo Parameter -DWITH_CDB enables CDB file storage support
 echo Parameter -DWITH_GUI to build GUI versions of bnetd, d2cs and d2dbs
-echo 
+echo ===================================================================
 echo Parameter clean deletes object files
-echo
-
+echo ===================================================================
 make -fMakefile.BORLAND %1 %2
