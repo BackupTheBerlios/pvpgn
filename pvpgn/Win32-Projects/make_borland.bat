0229dcbb11fa6cf30870679036f6908ab03ba7b4
pandaemonium <pandaemonium>
2004-10-26 20:46:57 +0000

    moved vorland makefile and batch file into Win32-Projects folder. console versions allready build fine, same for GUI version of bnetd. d2dbs and d2cs still cause trouble in GUI mode.
    compile with GUI by using make_BORLAND -DWITH_GUI

---
+++ pvpgn/Win32-Projects/make_borland.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Parameter -DWITH_SQL_MYSQL enables MySQL SQL storage support
+echo Parameter -DWITH_CDB enables CDB file storage support
+echo Parameter -DWITH_GUI to build GUI versions of bnetd, d2cs and d2dbs
+echo 
+echo Parameter clean deletes object files
+echo
+
+make -fMakefile.BORLAND %1 %2
--- pvpgn/src/make_borland.bat
+++
@@ -1,3 +0,0 @@
-#Parameter -DWITH_SQL_MYSQL enables MySQL SQL storage support
-#Parameter clean deletes object files
-make -fMakefile.BORLAND %1 %2
