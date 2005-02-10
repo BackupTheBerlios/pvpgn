4affaf2f478a60524a504856d26f562574323134
pandaemonium <pandaemonium>
2005-02-10 08:31:08 +0000

    updating some autoconf stuff

--- pvpgn/src/autoconf/ansi2knr.c
+++ pvpgn/src/autoconf/ansi2knr.c
@@ -1,6 +1,6 @@
-/* Copyright (C) 1989, 1997, 1998 Aladdin Enterprises.  All rights reserved. */
+/* Copyright (C) 1989, 2000 Aladdin Enterprises.  All rights reserved. */
 
-/*$Id: ansi2knr.c,v 1.1 2002/12/27 08:45:08 dizzy Exp $*/
+/*$Id: ansi2knr.c,v 1.2 2005/02/10 08:31:08 pandaemonium Exp $*/
 /* Convert ANSI C function definitions to K&R ("traditional C") syntax */
 
 /*
@@ -36,27 +36,63 @@ program under the GPL.
  * If no output_file is supplied, output goes to stdout.
  * There are no error messages.
  *
- * ansi2knr recognizes function definitions by seeing one or more
- * identifiers at the left margin, possibly including function pointer
- * types, eventually followed by a left parenthesis with a right
- * parenthesis after that and with a left brace as the first token on
- * the next line.  ansi2knr will recognize a multi-line header provided
- * that no intervening line ends with a left or right brace or a
- * semicolon.  These algorithms ignore whitespace and comments.
- * The following constructs will confuse it:
+ * ansi2knr recognizes function definitions by seeing a non-keyword
+ * identifier at the left margin, followed by a left parenthesis, with a
+ * right parenthesis as the last character on the line, and with a left
+ * brace as the first token on the following line (ignoring possible
+ * intervening comments and/or preprocessor directives), except that a line
+ * consisting of only
+ *	identifier1(identifier2)
+ * will not be considered a function definition unless identifier2 is
+ * the word "void", and a line consisting of
+ *	identifier1(identifier2, <<arbitrary>>)
+ * will not be considered a function definition.
+ * ansi2knr will recognize a multi-line header provided that no intervening
+ * line ends with a left or right brace or a semicolon.  These algorithms
+ * ignore whitespace, comments, and preprocessor directives, except that
+ * the function name must be the first thing on the line.  The following
+ * constructs will confuse it:
  *	- Any other construct that starts at the left margin and
  *	    follows the above syntax (such as a macro or function call).
- *	- Some macros that tinker with the syntax of the function header.
+ *	- Some macros that tinker with the syntax of function headers.
  */
 
 /*
  * The original and principal author of ansi2knr is L. Peter Deutsch
  * <ghost@aladdin.com>.  Other authors are noted in the change history
  * that follows (in reverse chronological order):
-	rac 2000-08-02 modified to not force the return values to be
-		on the preceding line, to transform prototypes, to
-		remove the "void" single parameter restriction, and to
-		handle function pointers with ANSI style parameters.
+
+	lpd 2000-04-12 backs out Eggert's changes because of bugs:
+	- concatlits didn't declare the type of its bufend argument;
+	- concatlits didn't recognize when it was inside a comment;
+	- scanstring could scan backward past the beginning of the string; when
+	- the check for \ + newline in scanstring was unnecessary.
+
+	2000-03-05  Paul Eggert  <eggert@twinsun.com>
+
+	Add support for concatenated string literals.
+	* ansi2knr.c (concatlits): New decl.
+	(main): Invoke concatlits to concatenate string literals.
+	(scanstring): Handle backslash-newline correctly.  Work with
+	character constants.  Fix bug when scanning backwards through
+	backslash-quote.  Check for unterminated strings.
+	(convert1): Parse character constants, too.
+	(appendline, concatlits): New functions.
+	* ansi2knr.1: Document this.
+
+	lpd 1999-08-17 added code to allow preprocessor directives
+		wherever comments are allowed
+	lpd 1999-04-12 added minor fixes from Pavel Roskin
+		<pavel_roskin@geocities.com> for clean compilation with
+		gcc -W -Wall
+	lpd 1999-03-22 added hack to recognize lines consisting of
+		identifier1(identifier2, xxx) as *not* being procedures
+	lpd 1999-02-03 made indentation of preprocessor commands consistent
+	lpd 1999-01-28 fixed two bugs: a '/' in an argument list caused an
+		endless loop; quoted strings within an argument list
+		confused the parser
+	lpd 1999-01-24 added a check for write errors on the output,
+		suggested by Jim Meyering <meyering@ascend.com>
 	lpd 1998-11-09 added further hack to recognize identifier(void)
 		as being a procedure
 	lpd 1998-10-23 added hack to recognize lines consisting of
@@ -152,19 +188,24 @@ program under the GPL.
 
 #endif
 
+/* Define NULL (for *very* old compilers). */
+#ifndef NULL
+# define NULL (0)
+#endif
+
 /*
  * The ctype macros don't always handle 8-bit characters correctly.
  * Compensate for this here.
  */
 #ifdef isascii
-#  undef HAVE_ISASCII		/* just in case */
-#  define HAVE_ISASCII 1
+# undef HAVE_ISASCII		/* just in case */
+# define HAVE_ISASCII 1
 #else
 #endif
 #if STDC_HEADERS || !HAVE_ISASCII
-#  define is_ascii(c) 1
+# define is_ascii(c) 1
 #else
-#  define is_ascii(c) isascii(c)
+# define is_ascii(c) isascii(c)
 #endif
 
 #define is_space(c) (is_ascii(c) && isspace(c))
@@ -176,13 +217,13 @@ program under the GPL.
 #define isidfirstchar(ch) (is_alpha(ch) || (ch) == '_')
 
 /* Forward references */
-static char *skipspace();
-static int writeblanks();
-static int test1();
-static int convert1();
-
-static char *startfn;
-static char *endfn;
+char *ppdirforward();
+char *ppdirbackward();
+char *skipspace();
+char *scanstring();
+int writeblanks();
+int test1();
+int convert1();
 
 /* The main program */
 int
@@ -192,6 +233,8 @@ main(argc, argv)
 {	FILE *in = stdin;
 	FILE *out = stdout;
 	char *filename = 0;
+	char *program_name = argv[0];
+	char *output_name = 0;
 #define bufsize 5000			/* arbitrary size */
 	char *buf;
 	char *line;
@@ -207,6 +250,7 @@ main(argc, argv)
 	 * check for this switch for backward compatibility.
 	 */
 	int convert_varargs = 1;
+	int output_error;
 
 	while ( argc > 1 && argv[1][0] == '-' ) {
 	  if ( !strcmp(argv[1], "--varargs") ) {
@@ -221,7 +265,8 @@ main(argc, argv)
 	    argv += 2;
 	    continue;
 	  }
-	  fprintf(stderr, "Unrecognized switch: %s\n", argv[1]);
+	  fprintf(stderr, "%s: Unrecognized switch: %s\n", program_name,
+		  argv[1]);
 	  fprintf(stderr, usage);
 	  exit(1);
 	}
@@ -231,16 +276,19 @@ main(argc, argv)
 		fprintf(stderr, usage);
 		exit(0);
 	case 3:
-		out = fopen(argv[2], "w");
+		output_name = argv[2];
+		out = fopen(output_name, "w");
 		if ( out == NULL ) {
-		  fprintf(stderr, "Cannot open output file %s\n", argv[2]);
+		  fprintf(stderr, "%s: Cannot open output file %s\n",
+			  program_name, output_name);
 		  exit(1);
 		}
 		/* falls through */
 	case 2:
 		in = fopen(argv[1], "r");
 		if ( in == NULL ) {
-		  fprintf(stderr, "Cannot open input file %s\n", argv[1]);
+		  fprintf(stderr, "%s: Cannot open input file %s\n",
+			  program_name, argv[1]);
 		  exit(1);
 		}
 		if ( filename == 0 )
@@ -252,14 +300,18 @@ main(argc, argv)
 	if ( filename )
 	  fprintf(out, "#line 1 \"%s\"\n", filename);
 	buf = malloc(bufsize);
+	if ( buf == NULL )
+	   {
+		fprintf(stderr, "Unable to allocate read buffer!\n");
+		exit(1);
+	   }
 	line = buf;
 	while ( fgets(line, (unsigned)(buf + bufsize - line), in) != NULL )
 	   {
 test:		line += strlen(line);
 		switch ( test1(buf) )
 		   {
-		case 3:			/* a function pointer */
-		case 2:			/* a function prototype */
+		case 2:			/* a function header */
 			convert1(buf, out, 1, convert_varargs);
 			break;
 		case 1:			/* a function */
@@ -269,10 +321,10 @@ f:			if ( line >= buf + (bufsize - 1) ) /* overflow check */
 			  goto wl;
 			if ( fgets(line, (unsigned)(buf + bufsize - line), in) == NULL )
 			  goto wl;
-			switch ( *skipspace(more, 1) )
+			switch ( *skipspace(ppdirforward(more), 1) )
 			  {
 			  case '{':
-			    /* Definitely a function */
+			    /* Definitely a function header. */
 			    convert1(buf, out, 0, convert_varargs);
 			    fputs(more, out);
 			    break;
@@ -303,41 +355,98 @@ wl:			fputs(buf, out);
 	if ( line != buf )
 	  fputs(buf, out);
 	free(buf);
-	if ( out != stdout )
-	  fclose(out);
+	if ( output_name ) {
+	  output_error = ferror(out);
+	  output_error |= fclose(out);
+	} else {		/* out == stdout */
+	  fflush(out);
+	  output_error = ferror(out);
+	}
+	if ( output_error ) {
+	  fprintf(stderr, "%s: error writing to %s\n", program_name,
+		  (output_name ? output_name : "stdout"));
+	  exit(1);
+	}
 	if ( in != stdin )
 	  fclose(in);
 	return 0;
 }
 
+/*
+ * Skip forward or backward over one or more preprocessor directives.
+ */
+char *
+ppdirforward(p)
+    char *p;
+{
+    for (; *p == '#'; ++p) {
+	for (; *p != '\r' && *p != '\n'; ++p)
+	    if (*p == 0)
+		return p;
+	if (*p == '\r' && p[1] == '\n')
+	    ++p;
+    }
+    return p;
+}
+char *
+ppdirbackward(p, limit)
+    char *p;
+    char *limit;
+{
+    char *np = p;
 
-/* Skip over space and comments, in either direction. */
-static char *
+    for (;; p = --np) {
+	if (*np == '\n' && np[-1] == '\r')
+	    --np;
+	for (; np > limit && np[-1] != '\r' && np[-1] != '\n'; --np)
+	    if (np[-1] == 0)
+		return np;
+	if (*np != '#')
+	    return p;
+    }
+}
+
+/*
+ * Skip over whitespace, comments, and preprocessor directives,
+ * in either direction.
+ */
+char *
 skipspace(p, dir)
     char *p;
     int dir;			/* 1 for forward, -1 for backward */
-{	if (*p=='\0') return p;
-	for ( ; ; )
-	   {	while ( is_space(*p) )
-		  p += dir;
-		if ( !(*p == '/' && p[dir] == '*') )
-		  break;
-		p += dir;  p += dir;
-		while ( !(*p == '*' && p[dir] == '/') )
-		   {	if ( *p == 0 )
-			  return p;	/* multi-line comment?? */
-			p += dir;
-		   }
-		p += dir;  p += dir;
-	   }
-	return p;
+{
+    for ( ; ; ) {
+	while ( is_space(*p) )
+	    p += dir;
+	if ( !(*p == '/' && p[dir] == '*') )
+	    break;
+	p += dir;  p += dir;
+	while ( !(*p == '*' && p[dir] == '/') ) {
+	    if ( *p == 0 )
+		return p;	/* multi-line comment?? */
+	    p += dir;
+	}
+	p += dir;  p += dir;
+    }
+    return p;
+}
+
+/* Scan over a quoted string, in either direction. */
+char *
+scanstring(p, dir)
+    char *p;
+    int dir;
+{
+    for (p += dir; ; p += dir)
+	if (*p == '"' && p[-dir] != '\\')
+	    return p + dir;
 }
 
 /*
  * Write blanks over part of a string.
  * Don't overwrite end-of-line characters.
  */
-static int
+int
 writeblanks(start, end)
     char *start;
     char *end;
@@ -354,99 +463,78 @@ writeblanks(start, end)
  * Return as follows:
  *	0 - definitely not a function definition;
  *	1 - definitely a function definition;
- *	2 - definitely a function prototype;
- *	3 - definitely a function pointer;
+ *	2 - definitely a function prototype (NOT USED);
  *	-1 - may be the beginning of a function definition,
  *		append another line and look again.
+ * The reason we don't attempt to convert function prototypes is that
+ * Ghostscript's declaration-generating macros look too much like
+ * prototypes, and confuse the algorithms.
  */
-static int
+int
 test1(buf)
     char *buf;
-{	char *p;
+{	char *p = buf;
 	char *bend;
+	char *endfn;
 	int contin;
 
-	if ( !isalpha(*buf) )
+	if ( !isidfirstchar(*p) )
 	  return 0;		/* no name at left margin */
-	bend = skipspace(buf + strlen(buf) - 1, -1);
+	bend = skipspace(ppdirbackward(buf + strlen(buf) - 1, buf), -1);
 	switch ( *bend )
 	   {
-	   case ';': contin = 2; break;
+	   case ';': contin = 0 /*2*/; break;
 	   case ')': contin = 1; break;
-	   case '{':
-	   case '}':
-	     return 0;		/* not a function */
+	   case '{': return 0;		/* not a function */
+	   case '}': return 0;		/* not a function */
 	   default: contin = -1;
 	   }
-
-	/* find start and end of function name */
-	startfn=buf;
-	while (*startfn!='\0' && *startfn!='(')
-	  {
-	    startfn++;
-	    startfn = skipspace(startfn, 1);
-	  }
-	if (*startfn=='\0')
-	    return 0;		/* not a function */
-	startfn--;
-	p = startfn;
-	startfn = skipspace(startfn,-1);
-	endfn = startfn+1;
-	while (isidchar(*startfn)) startfn--;
-	startfn++;
-	if (startfn>=endfn)
-	    return 0;
-	
-	p = skipspace(endfn, 1);
-#if 0
-/*                   v         v */
-/* typedef __ssize_t __io_read_  */
-fprintf(stderr,"found %s\n",buf);
-fprintf(stderr,"func=");
-{ int i; for (i=0; i<endfn-startfn; i++) fprintf(stderr,"%c",startfn[i]); }
-fprintf(stderr,"\n");
-fprintf(stderr,"p=%s\n",p);
-#endif
-	p++; /* skip ( */
+	while ( isidchar(*p) )
+	  p++;
+	endfn = p;
 	p = skipspace(p, 1);
-	if (*p=='*')  /* maybe a function pointer */
-	  {
-	    startfn = p;
-	    while (*p!='\0' && *p!=')')
-	        p++;
-	    if (*p=='\0')
-		return -1;  /* wraps to the next line */
-	    endfn = p-1;
-	    if (contin!=-1) contin = 3;
- 	  }
-	else
-	    if (!isidfirstchar(*p))
-	        return 0;  /* ok... no parameters anyway */
-	
+	if ( *p++ != '(' )
+	  return 0;		/* not a function */
+	p = skipspace(p, 1);
+	if ( *p == ')' )
+	  return 0;		/* no parameters */
 	/* Check that the apparent function name isn't a keyword. */
 	/* We only need to check for keywords that could be followed */
 	/* by a left parenthesis (which, unfortunately, is most of them). */
-	if (contin != 3)
-	{  static char *words[] =
-	     {	"asm", "auto", "case", "char", "const", "double",
-		"extern", "float", "for", "if", "int", "long",
-		"register", "return", "short", "signed", "sizeof",
-		"static", "switch", "typedef", "unsigned",
-		"void", "volatile", "while", 0
-	     };
-	  char **key = words;
-	  char *kp;
-	  int len = endfn - startfn;
+	   {	static char *words[] =
+		   {	"asm", "auto", "case", "char", "const", "double",
+			"extern", "float", "for", "if", "int", "long",
+			"register", "return", "short", "signed", "sizeof",
+			"static", "switch", "typedef", "unsigned",
+			"void", "volatile", "while", 0
+		   };
+		char **key = words;
+		char *kp;
+		unsigned len = endfn - buf;
 
-	  while ( (kp = *key) != 0 )
-	    {	if ( strlen(kp) == len && !strncmp(kp, startfn, len) )
-		{
-		  fprintf(stderr,"it was a keyword\n");
-		  return 0;	/* name is a keyword */
-		}
-		key++;
-	    }
-	}
+		while ( (kp = *key) != 0 )
+		   {	if ( strlen(kp) == len && !strncmp(kp, buf, len) )
+			  return 0;	/* name is a keyword */
+			key++;
+		   }
+	   }
+	   {
+	       char *id = p;
+	       int len;
+	       /*
+		* Check for identifier1(identifier2) and not
+		* identifier1(void), or identifier1(identifier2, xxxx).
+		*/
+
+	       while ( isidchar(*p) )
+		   p++;
+	       len = p - id;
+	       p = skipspace(p, 1);
+	       if (*p == ',' ||
+		   (*p == ')' && (len != 4 || strncmp(id, "void", 4)))
+		   )
+		   return 0;	/* not a function */
+	   }
 	/*
 	 * If the last significant character was a ), we need to count
 	 * parentheses, because it might be part of a formal parameter
@@ -455,7 +543,7 @@ fprintf(stderr,"p=%s\n",p);
 	if (contin > 0) {
 	    int level = 0;
 
-	    for (p = endfn; *p; p = skipspace(p + 1, 1))
+	    for (p = skipspace(buf, 1); *p; p = skipspace(p + 1, 1))
 		level += (*p == '(' ? 1 : *p == ')' ? -1 : 0);
 	    if (level > 0)
 		contin = -1;
@@ -464,13 +552,13 @@ fprintf(stderr,"p=%s\n",p);
 }
 
 /* Convert a recognized function definition or header to K&R syntax. */
-static int
+int
 convert1(buf, out, header, convert_varargs)
     char *buf;
     FILE *out;
     int header;			/* Boolean */
     int convert_varargs;	/* Boolean */
-{
+{	char *endfn;
 	char *p;
 	/*
 	 * The breaks table contains pointers to the beginning and end
@@ -483,17 +571,16 @@ convert1(buf, out, header, convert_varargs)
 	char **ap;
 	char *vararg = 0;
 
-	for (p=buf; p<startfn; p++)
-	  fprintf(out,"%c",*p);
-	
-        while (*(endfn++) != '(');
-	
+	/* Pre-ANSI implementations don't agree on whether strchr */
+	/* is called strchr or index, so we open-code it here. */
+	for ( endfn = buf; *(endfn++) != '('; )
+	  ;
 top:	p = endfn;
 	breaks = (char **)malloc(sizeof(char *) * num_breaks * 2);
-	if ( breaks == 0 )
+	if ( breaks == NULL )
 	   {	/* Couldn't allocate break table, give up */
 		fprintf(stderr, "Unable to allocate break table!\n");
-		fputs(startfn, out);
+		fputs(buf, out);
 		return -1;
 	   }
 	btop = breaks + num_breaks * 2 - 2;
@@ -502,7 +589,7 @@ top:	p = endfn;
 	do
 	   {	int level = 0;
 		char *lp = NULL;
-		char *rp;
+		char *rp = NULL;
 		char *end = NULL;
 
 		if ( bp >= btop )
@@ -529,14 +616,18 @@ top:	p = endfn;
 				else rp = p;
 				break;
 			   case '/':
-				p = skipspace(p, 1) - 1;
+				if (p[1] == '*')
+				    p = skipspace(p, 1) - 1;
 				break;
+			   case '"':
+			       p = scanstring(p, 1) - 1;
+			       break;
 			   default:
 				;
 			   }
 		   }
-		/* Erase any embedded function pointer parameters. */
-		if ( lp )
+		/* Erase any embedded prototype parameters. */
+		if ( lp && rp )
 		  writeblanks(lp + 1, rp);
 		p--;			/* back up over terminator */
 		/* Find the name being declared. */
@@ -552,14 +643,24 @@ top:	p = endfn;
 				while ( level )
 				 switch ( *--p )
 				   {
-				   case ']': case ')': level++; break;
-				   case '[': case '(': level--; break;
-				   case '/': p = skipspace(p, -1) + 1; break;
+				   case ']': case ')':
+				       level++;
+				       break;
+				   case '[': case '(':
+				       level--;
+				       break;
+				   case '/':
+				       if (p > buf && p[-1] == '*')
+					   p = skipspace(p, -1) + 1;
+				       break;
+				   case '"':
+				       p = scanstring(p, -1) + 1;
+				       break;
 				   default: ;
 				   }
 			   }
 				if ( *p == '(' && *skipspace(p + 1, 1) == '*' )
-				   {	/* We found the name of the function pointer */
+				   {	/* We found the name being declared */
 					while ( !isidfirstchar(*p) )
 					  p = skipspace(p, 1) + 1;
 					goto found;
@@ -603,7 +704,8 @@ found:		if ( *p == '.' && p[-1] == '.' && p[-2] == '.' )
 		   }
 	   }
 	/* Put out the function name and left parenthesis. */
-	for (p=startfn; p != endfn; p++) putc(*p, out);
+	p = buf;
+	while ( p != endfn ) putc(*p, out), p++;
 	/* Put out the declaration. */
 	if ( header )
 	  {	fputs(");", out);
--- pvpgn/src/autoconf/termios.m4
+++ pvpgn/src/autoconf/termios.m4
@@ -1,8 +1,27 @@
-dnl From Jim Meyering.
+# AM_SYS_POSIX_TERMIOS
+
+# Copyright 1996, 2000, 2001 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+# From Jim Meyering.
 
 # serial 1
 
-AC_DEFUN(AM_SYS_POSIX_TERMIOS,
+AC_DEFUN([AM_SYS_POSIX_TERMIOS],
 [AC_CACHE_CHECK([POSIX termios], am_cv_sys_posix_termios,
   [AC_TRY_LINK([#include <sys/types.h>
 #include <unistd.h>
