99acb56b207addb24196daadaac7211920446aad
d1zzy <d1zzy>
2005-03-27 12:13:27 +0000

    Changed the files2db scripts instead of talking directly to the db backend (and thus only supporting the backends someone writes support for) to dump the SQL statements that should be executed to stdout (the other status messages are now dumped to stderr so you can use piping or redirection to get the SQL statements). So now you could use this on any SQL backend that understand those commands (all should). Notice however that clans and (the new) teams are not converted, they are not stored in the general account attributes way, so they need special conversion codes.

--- pvpgn/scripts/sql/files2db.pl
+++ pvpgn/scripts/sql/files2db.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/perl
 
 
-require "db_api.pl";
-
-if (scalar(@ARGV) != 5) {
+if (scalar(@ARGV) != 1) {
     &usage;
     exit;
 }
@@ -14,8 +12,6 @@ if (scalar(@ARGV) != 5) {
 $dirpath = $ARGV[0];
 $dirpath =~ s!(.*)/$!$1!;
 
-$dbh = &db_init($ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4]);
-
 opendir FILEDIR, $dirpath or die "Error opening filedir!\n";
 
 while ($filename = readdir FILEDIR) {
@@ -24,26 +20,26 @@ while ($filename = readdir FILEDIR) {
     
     if ( ! (($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
     $atime,$mtime,$ctime,$blksize,$blocks) = stat "$dirpath/$filename" )) {
-        print "Error stat-ing the file $pathdir/$filename!\n" ; next; }
+        print STDERR "Error stat-ing the file $pathdir/$filename!\n" ; next; }
     
     $type = ($mode & 070000) >> 12;
     
     if ($type != 0) {
-	print "File $dirpath/$filename its not regular\n";
+	print STDERR "File $dirpath/$filename its not regular\n";
 	next;
     }
-    convertfile2db("$dirpath/$filename");
+    convertplain2db("$dirpath/$filename");
 }
 
 closedir FILEDIR;
 
-sub convertfile2db {
+sub convertplain2db {
     my $filen = shift;
     my ($userid, $count, $alist);
     
     open FILE, $filen or die "Error opening file $filen\n";
-    print "Converting file $filen ... ";
-    
+    print STDERR "Converting file $filen ... ";
+
     $count = 0;
     $userid = ""; undef @alist;
     while($line = <FILE>) {
@@ -67,21 +63,47 @@ sub convertfile2db {
 	&db_set($dbh, $userid, $alist);
     }
     close FILE;
-    print "done\n";
+    print STDERR "done\n";
 }
 
 sub header {
-    print "Account files to db accounts converting tool.\n";
-    print " Copyright (C) 2002 Dizzy (dizzy\@roedu.net)\n";
-    print " People Versus People Gaming Network (www.pvpgn.org)\n\n";
+    print STDERR "Account files to db accounts converting tool.\n";
+    print STDERR " Copyright (C) 2002,2005 Dizzy (dizzy\@roedu.net)\n";
+    print STDERR " Player Versus Player Gaming Network (www.pvpgn.org)\n\n";
 }
 
 sub usage {
     &header;
-    print "Usage:\n\n\tfiles2db.pl <filedir> <dbhost> <dbname> <dbuser> <dbpass>\n\n";
-    print "\t <filedir>\t: directory with the account files\n";
-    print "\t <dbhost>\t: the database host\n";
-    print "\t <dbname>\t: the database name\n";
-    print "\t <dbuser>\t: the database user\n";
-    print "\t <dbpass>\t: the database password\n";
+    print STDERR "Usage:\n\n\tfiles2db.pl <filedir>\n\n";
+    print STDERR "\t <filedir>\t: directory with the account files\n";
+}
+
+sub db_set {
+    my $dbh = shift;
+    my $userid = shift;
+    my $alist = shift;
+    my ($i);
+
+    print("INSERT INTO BNET (uid) VALUES ($userid);\n");
+    print("INSERT INTO profile (uid) VALUES ($userid);\n");
+    print("INSERT INTO Record (uid) VALUES ($userid);\n");
+    print("INSERT INTO friend (uid) VALUES ($userid);\n");
+    for($i=0; $i<scalar(@alist);$i++) {
+        my $tab = $alist[$i]{tab};
+        my $col = $alist[$i]{col};
+        my $val = $alist[$i]{val};
+
+        $nval = &add_slashes($val);
+        print("ALTER TABLE $tab ADD COLUMN '$col' VARCHAR(128);\n");
+        print("UPDATE $tab SET '$col' = '$nval' WHERE uid = $userid;\n");
+    }
+}
+
+sub add_slashes {
+    my $str = shift;
+
+    $str =~ s/\\/\\\\/g;
+    $str =~ s/\'/\\\'/g;
+
+    return $str;
 }
