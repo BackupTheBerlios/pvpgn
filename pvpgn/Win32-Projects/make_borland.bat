d806209cd4e08b212c0b338b0532e04015da9dff
pandaemonium <pandaemonium>
2005-05-13 20:11:26 +0000

    updated infos so you can build with ODBC support

--- pvpgn/Win32-Projects/make_borland.bat
+++ pvpgn/Win32-Projects/make_borland.bat
@@ -1,6 +1,7 @@
 @echo off
 echo ===================================================================
 echo Parameter -DWITH_SQL_MYSQL enables MySQL SQL storage support
+echo Parameter -DWITH_SQL_ODBC enables ODBC SQL storage support
 echo Parameter -DWITH_CDB enables CDB file storage support
 echo Parameter -DWITH_GUI to build GUI versions of bnetd, d2cs and d2dbs
 echo ===================================================================
